49e426936ad2d1d248fd8a9d33522d21eda96640
RichardKim <hiddeneventor@gmail.com>
2022-10-11 21:54:43 +0900

    repository Test 환경 세팅 완료

--- src/main/java/shop/hiddenevent/junitproject/repository/BookRepository.java
+++ src/main/java/shop/hiddenevent/junitproject/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package shop.hiddenevent.junitproject.repository;
 
-public class BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import shop.hiddenevent.junitproject.domain.Book;
+
+public interface BookRepository extends JpaRepository<Book, String> {
 }
