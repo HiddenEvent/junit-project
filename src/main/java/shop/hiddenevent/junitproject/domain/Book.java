ad5451aa40877830b799fb6e4833dfc6b6f87c52
RichardKim <hiddeneventor@gmail.com>
2022-10-11 21:41:14 +0900

    generate용 공통 코드 추가 테스트용 Entity 추가

--- src/main/java/shop/hiddenevent/junitproject/domain/Book.java
+++ src/main/java/shop/hiddenevent/junitproject/domain/Book.java
@@ -1,4 +1,21 @@
 package shop.hiddenevent.junitproject.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Getter
+@Entity
 public class Book {
+    @Id
+    @Column(length = 32, nullable = false)
+    private String id;
+    @Column(length = 50, nullable = false)
+    private String title;
+    @Column(length = 20, nullable = false)
+    private String author;
 }
