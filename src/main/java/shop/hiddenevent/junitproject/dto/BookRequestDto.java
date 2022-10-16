60971141f53c8352bd139601287844be15cdfe4a
RichardKim <hiddeneventor@gmail.com>
2022-10-16 16:02:06 +0900

    책등록 Service 테스트 완료 -> repository 가짜 객체로 추후 변경

--- src/main/java/shop/hiddenevent/junitproject/dto/BookRequestDto.java
+++ src/main/java/shop/hiddenevent/junitproject/dto/BookRequestDto.java
@@ -1,18 +1,21 @@
 package shop.hiddenevent.junitproject.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class BookRequestDto {
 
     @Getter
+    @Setter
     public static class Create {
         private String title;
         private String author;
     }
 
     @Getter
+    @Setter
     public static class Modify {
         private String title;
         private String author;
--- src/test/java/shop/hiddenevent/junitproject/service/BookServiceTest.java
+++ src/test/java/shop/hiddenevent/junitproject/service/BookServiceTest.java
@@ -1,7 +1,39 @@
 package shop.hiddenevent.junitproject.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import shop.hiddenevent.junitproject.dto.BookRequestDto;
+import shop.hiddenevent.junitproject.dto.BookResponseDto;
+import shop.hiddenevent.junitproject.repository.BookRepository;
+import shop.hiddenevent.junitproject.util.mail.MailSenderStub;
 
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
 class BookServiceTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void 책등록하기_테스트(){
+        //given
+        BookRequestDto.Create dto = new BookRequestDto.Create();
+        dto.setTitle("제목이다");
+        dto.setAuthor("리차드킴이다.");
+
+        // stub
+        MailSenderStub mailSenderStub = new MailSenderStub();
+        // 각짜 Repository 만들기
+
+        // when
+        BookService bookService = new BookService(bookRepository, mailSenderStub);
+        BookResponseDto.Create responseDto = bookService.createBook(dto);
+
+        // then
+        assertEquals(dto.getTitle(), responseDto.getTitle());
+        assertEquals(dto.getAuthor(), responseDto.getAuthor());
+
+    }
 
 }
